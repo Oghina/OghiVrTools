91b632447920041d040bf6fcad7d67f7d604eb2c
Oghina <stefan.oghina@yahoo.com>
2025-02-26 14:00:17 +0200

    Update package.json and OghiVrTools script

--- OghiVrTools.cs
+++ OghiVrTools.cs
@@ -6,7 +6,8 @@ namespace OghiVrTools
     public static class OghiVrTools 
     {
         private static readonly string uiCanvasPrefabLocation = "Packages/com.oghina.oghivrtools/UI/Prefabs/VR_Canvas.prefab";
-
+        private static readonly string packageFolderLocation = "Packages/com.oghina.oghivrtools/";
+        
         public static void InitializeVrTools()
         {
             GameObject prefab = (GameObject)AssetDatabase.LoadAssetAtPath(uiCanvasPrefabLocation, typeof(GameObject));
@@ -17,6 +18,13 @@ namespace OghiVrTools
             else
             {
                 Debug.LogError("Failed to load prefab: " + uiCanvasPrefabLocation);
+            }
+            
+            string[] guids = AssetDatabase.FindAssets("", new[] { packageFolderLocation });
+            foreach (string guid in guids)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                Debug.Log("Asset found: " + assetPath);
             }
         }
     }
